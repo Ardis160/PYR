3098d7ca04b43c51fd1cc56996d346794e98940c
Ardis160 <andrewpatro@seznam.cz>
2024-10-12 22:18:48 +0200

    email regex

--- cisteni.py
+++ cisteni.py
@@ -3,13 +3,13 @@ import pandas as pd
 df = pd.read_csv("catalog.csv")
 
 hlavicka = df.head()
-#print(df.isnull().sum())
+print(df.isnull().sum())
 
 #strategie reseni chybejicich hodnot
 
 #smazani radku/sloupcu s chybejicimi zaznamy
 df2 = df.dropna(axis=1)
-#print(df2)
+print(df2)
 
 #doplnovani chybejicich zaanamu
 df.fillna(0)
@@ -23,4 +23,4 @@ from sklearn.impute import SimpleImputer
 imputer = SimpleImputer
 dfnum = df.select_dtypes(include='number')
 df3 = imputer.fit_transform(dfnum)
-print(df3)
+#print(df3)
